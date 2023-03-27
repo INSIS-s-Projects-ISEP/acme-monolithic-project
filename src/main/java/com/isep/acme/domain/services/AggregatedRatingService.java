9150061520ab065c015ec576862543bc98dbb86b
Regiana Barbosa <regiana.cruz@acad.ifma.edu.br>
2023-03-27 16:39:29 +0100

    AggregateReview deleted

--- src/main/java/com/isep/acme/domain/services/AggregatedRatingService.java
+++
@@ -1,8 +0,0 @@
-package com.isep.acme.domain.services;
-
-import com.isep.acme.domain.model.AggregatedRating;
-
-public interface AggregatedRatingService {
-
-    AggregatedRating save(String sku);
-}
